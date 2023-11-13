2889f2e6d2cdc1987748832cead4739010174700
Gabriel Castro de Souza <112295661+gabrielprogramadorweb@users.noreply.github.com>
2023-11-13 16:15:40 -0300

    att

--- index.php
+++ index.php
@@ -9,12 +9,12 @@ require_once('../atividade-1/class/RomanoParaInteiro.php');
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <!-- Configuração da meta e título -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Conversor Romano</title>
+    <title>Conversor Numérico</title>
     <link rel="icon" href="../atividade-1/imagens/favicon.ico" type="image/x-icon">
     <!-- Inclusão do Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
